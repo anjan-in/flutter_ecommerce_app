aa87d4ec6c8d35f5be4929c177a3080a3de6d49e
anjan-in <anjansen30@gmail.com>
2025-04-22 18:58:42 +0530

    Re-initializing project from start

--- lib/providers/product_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import '../models/product_model.dart';
-import '../core/dummy_data.dart';
-
-class ProductProvider with ChangeNotifier {
-  List<Product> _items = dummyProducts;
-
-  List<Product> get items => _items;
-}
